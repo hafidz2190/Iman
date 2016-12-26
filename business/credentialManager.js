5db1ef0cc87adca8cad123354444f20f26e46a12
Hafidz Azhar <hafidz2190@gmail.com>
2016-12-26 22:14:41 +0700

    cleaned up

--- business/credentialManager.js
+++ business/credentialManager.js
@@ -20,32 +20,6 @@ function managerDefinitions()
                 data: results[0]
             };
         }
-
-        // function callback(results)
-        // {
-        //     var email = data.email;
-        //     var password = data.password;
-        //     var rows = results;
-
-        //     var matched = false;
-
-        //     for(var i = 0, ii = rows.length; i < ii; i++)
-        //     {
-        //         var row = rows[i];
-        //         var currentEmail = row['email'];
-        //         var currentPassword = row['password'];
-
-        //         if(email != currentEmail || password != currentPassword)
-        //             continue;
-
-        //         matched = true;
-        //         break;
-        //     }
-
-        //     return {
-        //         data: matched
-        //     };            
-        // }
     }
     
     return {
