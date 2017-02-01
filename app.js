21baef97c701aa25ec0a7e4ffa7f44714846ea75
Hafidz Azhar <hafidz2190@gmail.com>
2017-02-01 11:47:54 +0700

    refactored app.js

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var appConfig = require('./config.json');
+main();
 
-var userRouter = require('./routers/userRouter');
-
-var app = express();
-var port = appConfig.server.port;
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(allowCors);
-
-app.use('/user', userRouter);
-app.use(errorHandler);
-
-app.listen(port, listenCallback);
-
-function listenCallback()
+function main()
 {
-    console.log('iman app listening on port ' + port);
-}
+    var _appConfig = require('./config.json');
+    var _bodyParser = require('body-parser');
+    var _cookieParser = require('cookie-parser');
+    var _express = require('express');
 
-function allowCors(req, res, next)
-{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-}
+    var _userRouter = require('./routers/userRouter');
 
-function errorHandler(err, req, res, next)
-{
-    res.status(500);
-    res.send(err.stack);
+    var _app = _express();
+    var _port = _appConfig.server.port;
+
+    _app.use(_bodyParser.urlencoded({extended: true}));
+    _app.use(_bodyParser.json());
+    _app.use(_cookieParser());
+    _app.use(allowCors);
+
+    _app.use('/user', _userRouter);
+    _app.use(errorHandler);
+
+    _app.listen(_port, listenCallback);
+
+    function listenCallback()
+    {
+        console.log('iman app listening on port ' + _port);
+    }
+
+    function allowCors(req, res, next)
+    {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.header('Access-Control-Allow-Credentials', true);
+        next();
+    }
+
+    function errorHandler(err, req, res, next)
+    {
+        res.status(500);
+        res.send(err.stack);
+    }
 }
